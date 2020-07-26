46e5e7d84fd22a20007cb21498941c0270a7162e
unknown <luisfelipesdn12@gmail.com>
2020-07-26 14:45:13 -0300

    Accept cli argvs and multiple types of cube

--- main.py
+++ main.py
@@ -1,42 +1,58 @@
 from telegram.ext import Updater, CommandHandler
 from telegram import Bot
-import requests, random
+import requests, random, sys
 
 API_TOKEN = open("API_TOKEN.txt", "r").read()
-keep_going = False
+
+# > python main.py {keep_going:bool} {alert_groups:bool}
+
+try: keep_going = sys.argv[1].lower() == 'true'
+except: keep_going = False
+
+try: alert_groups = sys.argv[2].lower() == 'true'
+except: alert_groups = False
 
 groups_subscribed = [-1001250429838, -1001479660521]
 
-def get_scramble():
-    api_uri = "https://scrambler-api.herokuapp.com/3x3x3"
+def get_scramble(type):
+    api_uri = f"https://scrambler-api.herokuapp.com/{type}"
     response = requests.get(api_uri)
     scramble = random.choice(response.json())
     return(scramble)
 
 def scramble(update, context):
+    scramble = None
+    
+    try: cube_type = context.args[0]
+    except IndexError: cube_type = "3x3x3"
+
+    try: scramble = get_scramble(cube_type)
+    except: scramble = "Aparently this is not a valid cube type :/"
+    
     update.message.reply_text(
-        f'Hello {update.message.from_user.first_name}, your scramble is {get_scrambles()}'
+        f'Hello {update.message.from_user.first_name}, your {cube_type} scramble is: \n\n{scramble}'
         )
     print(context.user_data)
 
-def daily_example_scramble():
+def daily_example_scramble(type):
     message = (
 f"""
 *Example Solve Diária:*
-3x3x3
+{type}
 
 Embaralhe com o verde na frente e o branco em cima:
 
-{get_scramble()}
+{get_scramble(type)}
 """
     )
     return(message)
 
 bot = Bot(API_TOKEN)
 
-for group_sub in groups_subscribed:
-    print(group_sub)
-    bot.send_message(group_sub, text=daily_example_scramble(), parse_mode="Markdown")
+if alert_groups:
+    for group_sub in groups_subscribed:
+        print(group_sub)
+        bot.send_message(group_sub, text=daily_example_scramble("3x3x3"), parse_mode="Markdown")
 
 if keep_going == True:
     updater = Updater(API_TOKEN, use_context=True)

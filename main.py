db39c5e2fa3bc629292d38cd519bc6bf8f43d6d1
unknown <luisfelipesdn12@gmail.com>
2020-08-03 22:58:37 -0300

    Multiple scrambles argument added

--- main.py
+++ main.py
@@ -20,17 +20,54 @@ def get_scramble(type):
     scramble = random.choice(response.json())
     return(scramble)
 
+def get_n_scrambles(type, n):
+    api_uri = f"https://scrambler-api.herokuapp.com/{type}"
+
+    scrambles_list = list()
+
+    while len(scrambles_list) < n:
+        five_scrambles = requests.get(api_uri).json()
+        scrambles_list += five_scrambles
+
+    while len(scrambles_list) > n:
+        scrambles_list.pop()
+
+    return(scrambles_list)
+
 def scramble(update, context):
-    scramble = None
-    
+
     try: cube_type = context.args[0]
     except IndexError: cube_type = "3x3x3"
 
-    try: scramble = get_scramble(cube_type)
-    except: scramble = f"Aparently this is not a valid cube type :/ \nOr something went wrong while I've tried to access the data in the API: \nhttps://scrambler-api.herokuapp.com/{cube_type}"
-    
+    try:
+        n_of_scrambles = int(context.args[1])
+        if n_of_scrambles > 25: n_of_scrambles = 25
+    except IndexError: n_of_scrambles = 1
+
+    error_message = f"Aparently this is not a valid cube type :/ \nOr something went wrong while I've tried to access the data in the API: \nhttps://scrambler-api.herokuapp.com/{cube_type}"
+
+    def return_scrambles_formated():
+        if n_of_scrambles > 1:
+            try:
+                scramble = get_n_scrambles(cube_type, n_of_scrambles)
+                string_of_formated_scrambles = ""
+
+                for c in range(len(scramble)):
+                    string_of_formated_scrambles += f"*{c+1}*. {scramble[c]}\n\n"
+
+                return(string_of_formated_scrambles)
+            except:
+                return(error_message)
+
+        else:
+            try: scramble = get_scramble(cube_type)
+            except: scramble = error_message
+
+            return(scramble)
+
     update.message.reply_text(
-        f'Hello {update.message.from_user.first_name}, your {cube_type} scramble is: \n\n{scramble}'
+        f'Hello {update.message.from_user.first_name}, your {cube_type} scramble is: \n\n{return_scrambles_formated()}',
+        parse_mode="Markdown"
         )
     print(context.user_data)
 
